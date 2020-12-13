85abb23b5ab94505526175af477425dc93b8fa7b
git <wkaifeng2007@163.com>
2020-12-14 07:28:23 +0800

    update code

--- py3.code/BuffAutoConfig2Steam/Buff2Steam.py
+++ py3.code/BuffAutoConfig2Steam/Buff2Steam.py
@@ -1,81 +1,74 @@
 #-*-coding:utf8-*-
 
 import steam.webauth as wa
-import requests
 from requests import Session
 import re
 from decimal import Decimal
 import time
 from random import randint as randt
+from Naked.toolshed.shell import muterun_js
 import json
+from redis import Redis
 
 header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36 Aoyou/VnN1RG0qQ1hkKz0iT1h0WW39_ebQ7Og6EiMFYYb9HbCN9c3PheedkDIN'}
-COMMUNITY_URL = 'https://steamcommunity.com'
-CREATE_BUY_ORDER = '/market/createbuyorder/'
-GAME_DIC = {'STEAM': '753', 'CS': '730', 'DOTA': '570'}
-CURRENCY = {
-    'CNY': 23,
-    'USD': 1,
-    'EUR': 3,
-    'JPY': 8,
-    'AUD': 21,
-    'CAD': 20,
-    'RUB': 5,
-}
-SEARCH_URL = f'https://steamcommunity.com/market/search?q='
-
-
-buff_trade_url = 'https://buff.163.com/api/market/steam_trade?_=1606985806092'
 
 def buff163_login_auth():
-    cookie = {'Cookie': 'Device-Id=ksrKqrhnXI3IbCO86D7w; _ga=GA1.2.470761462.1607086108; mail_psc_fingerprint=45b1e99d012b773f63c65a533c20153a; _ntes_nnid=04439bfec1d965edc0aeb61194697258,1607086284303; _ntes_nuid=04439bfec1d965edc0aeb61194697258; Locale-Supported=zh-Hans; game=csgo; _gid=GA1.2.2082947366.1607442976; NTES_YD_SESS=bzDcqGpfyUd6cGFIMHqpDj.SAxYnzYdDQgp_h8PxII7miAPuiaN2.Hs6DijXG7OTHRmya5OhuWW6SCNgMTzYuaTT71pp_FVds7REJnJRQup3Cgh4zHq7IOHt1Zt3XZmiOeyqKH7Lps9sH5h838wS05drZ0YZOeHmcqQ4n9G.L..KUoEJ1LZo3uPByHwwopQeFKoLrK_4UGhpatwqwwAKWBCh9xRjLMxq_Q_S1yAffO.5d; S_INFO=1607528515|0|3&80##|13613905817; P_INFO=13613905817|1607528515|1|netease_buff|00&99|hen&1607047107&netease_buff#hen&410100#10#0#0|&0|null|13613905817; remember_me=U1094423637|oyRcOTztGVqpoxRAmKkkEzYlSsAdK6xr; session=1-DxLamp4SK5-YocjT3dCSxvBn3tcKQziNECWHALSEmknY2046102285; _gat_gtag_UA_109989484_1=1; csrf_token=IjRkZjlkMzk0NGRkNjdjOTc1YzNiNTVhN2QzOTBjNzg1NDJiMTZlM2Ii.ErKB6w.bhnA_CvC_tHsDHq1ZYkSRrs543M'}
-    buff_session = Session()
-    anthor = 'https://buff.163.com/api/market/steam_trade/batch/info?bot_trades=201209T1268028992&_=1607528632768'
-    trade_url = 'https://buff.163.com/market/tradeoffer/4330534345'
-    resp = buff_session.get(url='https://buff.163.com/api/market/steam_trade?_=1606985806092', headers=header, cookies=cookie).content.decode('utf-8')
-    print(resp)
-    json_resp = json.loads(resp)['data']
-    print(len(json_resp))
-    # for each_trade in json_resp:
-    #     steam_trade_url = each_trade['url']
-    #     verify_code = each_trade['verify-code'].split(' ')[1]
-    #     buyer_name = each_trade['bot_name']
-    #     buyer_datetime = each_trade['bot_extra_info']
-    #     game_id = each_trade['appid']
-    #     game_name = each_trade['game']
-    #     good_info = each_trade['items_to_trade']
-    # return trade_url
+    print('准备登录buff163......')
+    muterun_js('buff_login.js')
+    print('登录完成......')
+    # cookie = {'Cookie': 'Id=ksrKqrhnXI3IbCO86D7w; _ga=GA1.2.470761462.1607086108; mail_psc_fingerprint=45b1e99d012b773f63c65a533c20153a; _ntes_nnid=04439bfec1d965edc0aeb61194697258,1607086284303; _ntes_nuid=04439bfec1d965edc0aeb61194697258; Locale-Supported=zh-Hans; game=csgo; _gid=GA1.2.1096537323.1607878503; _gat_gtag_UA_109989484_1=1; NTES_YD_SESS=XrouU0N_.PqdNRe1rZfR0HBLLr_7FBeNj7vaUPyLU4jVAZ_WA7Kw6gH8JA0uTL1PgcVb7E1zWhh8nIKNmPStW7PPL5vvqjQ3HLcoYxYckWviINz9SgCLB1dWtrSxdcbQtQVXXRyypnLKgXa17C9T3iadf6lbk_jaSKjwPhfr9hOo0VNIsFwQpA7Szy_M4yimVFlzD2McqOZMaNdSUrWgE9fSEWDsBTr1v6s6z768CkOD3; S_INFO=1607878526|0|3&80##|13613905817; P_INFO=13613905817|1607878526|1|netease_buff|00&99|hen&1607528515&netease_buff#hen&410100#10#0#0|&0|null|13613905817; remember_me=U1094423637|jnrmtSRxKq6V60MTkoPAaNyLpkn83Zwe; session=1-p9xAC_fzWxoTw2bcjtwO2oyEtiFC1DPf7jPP898LuhLM2046102285; csrf_token=IjE3N2JkN2I0MzBjZjNlMmZhYzcxNDdkZTk1Nzk3ZjI5Y2EzZTc3YWMi.ErfZCQ.A-PY5D0ZMgCtI0QDCu6rY6nHydo'}
 
 
 def monitor_buff():
+    with open('cookie.json', 'r+') as f:
+        js_cook = f.read()
+    cookie = {'Cookie': js_cook}
     buff_session = Session()
+    steam_trade_list = []
     while True:
         resp = buff_session.get(url='https://buff.163.com/api/market/steam_trade?_=1606985806092', headers=header, cookies=cookie).content.decode('utf-8')
         if len(json.loads(resp)['data']) > 0:
+            data = json.loads(resp)['data']
             break
+        time.sleep(randt(1800, 7200))   # 每30分钟-2小时检测一次
+    for each_data in data:
+        steam_trade_list.append(
+            {
+                'steam_trade_id': each_data['tradeofferid'],
+                'join_steam_date': each_data['bot_extra_info'],
+                'game_id': each_data['appid'],
+                'verify_code': each_data['verify_code'].split(' ')[1],
+                'trade_item_info': each_data['items_to_trade']
+            }
+        )
+    return steam_trade_list
 
 
 def steam_login_auth(username, passwd):
-    # requests.
     user = wa.WebAuth(username, passwd)
-    session = user.login(twofactor_code=input('请输入 2FA Code:'), language='schinese')
-    return session
+    steam_session = user.login(twofactor_code=input('请输入 2FA Code:'), language='schinese')
+    return steam_session
 
-def deal_exchange(trade_id):
+
+def deal_exchange(steam_session, steam_trade_list):
+    trade_id = steam_trade_list['steam_trade_id']
     steam_trade_url = f'https://steamcommunity.com/tradeoffer/{trade_id}'
-    pass
+    resp = steam_session.get(url=steam_trade_url).content.decode('utf-8')
+    buyer_join_date = re.findall('(.*?)', resp)[0]
+    if buyer_join_date == steam_trade_list['join_steam_date']:
+        post_data = {
 
-def alarm():
-    pass
+        }
+        steam_session.post(url='', data=post_data)
+        print('成功确认交易请求，请在手机端确认......')
 
-def test_steam():
-    goods_info_url = 'https://steamcommunity.com/profiles/76561198179582527/inventory/json/730/2/?trading=1'
-    tar = 'https://steamcommunity.com/tradeoffer/4331700462'
-    cookie = {'Cookie': 'sessionid=6bced180bca8b8cc9900b6f7; steamCountry=JP%7C9ea4339f554b6c8f88fde3c1ea0a8849; timezoneOffset=28800,0; _ga=GA1.2.1405874360.1607442989; _gid=GA1.2.986278078.1607442989; steamMachineAuth76561198179582527=3B49C91B4A551B448557655D001C26D439B76179; steamRememberLogin=76561198179582527%7C%7C4974f82bd8298d21240f59cfa579120c; browserid=2150996258583710385; webTradeEligibility=%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22new_device_cooldown_days%22%3A7%2C%22time_checked%22%3A1607605399%7D; steamLoginSecure=76561198179582527%7C%7CF11A6E32ED22EF11BA1E2027B13470AE2DEAE30A'}
-    header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
-    resp = requests.get(url=tar, headers=header,cookies=cookie).content.decode('utf-8')
-    print(resp)
 
 if __name__ == '__main__':
-    # buff163_login_auth()
-    test_steam()
+    print('请先登录buff和steam账号......')
+    print('正在跳转......')
+    buff163_login_auth()
+    steam_session = steam_login_auth(input('请输入Steam账号：'), input('请输入Steam密码：'))
+    while True:
+        steam_trade_list = monitor_buff()
+        deal_exchange(steam_session, steam_trade_list)
+
--- py3.code/BuffAutoConfig2Steam/buff_login.js
+++ py3.code/BuffAutoConfig2Steam/buff_login.js
@@ -1,7 +1,8 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 var fs = require('fs');
-
-// const random = require('/home/antx/Code/SOC-TI/ti_ds/spider/base')
 
 (async ()=>{
     const browser = await puppeteer.launch({
@@ -17,62 +18,48 @@ var fs = require('fs');
             '--disable-bundled-ppapi-flash',
             '--mute-audio',
             '--disable-gpu',
-            // '--proxy-server=http://SOCANTXA0GARL0R0:OAMlIL3a@http-proxy-t3.dobel.cn:9180'
-        ]
+            '--disable-infobars'
+        ],
+        ignoreDefaultArgs:[
+            '--enable-automation'
+        ],
     });
     const page = await browser.newPage();               // 打开浏览器的一个新的page页面
-    // await page.emulate(devices['iPhone X']);         // 模拟手机iphone X
     try{
+        fs.access('cookie.json', fs.constants.F_OK | fs.constants.W_OK, (err) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log('Json-DB中cookie有效，直接进行监控......')
+            }
+        });
+    }catch (error){
+        await console.log('Json-DB中cookie已过期，请重新登录并获取......')
         await page.goto('https://buff.163.com/',);   // Input your target url
         await page.waitFor(3000);
         await page.click('[onclick="loginModule.showLogin()"]');
-        await console.log('点击登录');
-        // await page.waitForSelector('[id="phoneipt"]', {timeout:0});
-        await page.waitFor(3000);
-        await console.log('111');
-        await page.click('[class="tab0"]');
-        await console.log('使用密码验证登录');
-        const username = '13613905817';
-        const passwd = 'qq136571820';
-        await page.type('[class="dlemail j-nameforslide"]', username);
-        await page.type('[class="u-pwdtext"]', passwd);
-        await console.log('请滑动验证码');
-        await page.waitForSelector('[class="j_drop-handler"]', {timeout:0});
-        // await page.refresh
-    }catch (error){
-        console.log('请输入短信验证码，并滑动验证码')
-        // await page.click('[onclick="loginModule.showLogin()"]');
-        // await page.waitForSelector('[id="mobile-itl-div"]', {timeout:0});
+        await console.log('请点击登录， 并勾选10天免登陆......');
+        await page.waitForSelector('[class="j_drop-handler"]', {timeout:0});    // 登录成功后再进行后面的动作
     }finally {
-        // await page.click('[onclick="loginModule.showLogin()"]');
-        // await page.waitForSelector('[id="mobile-itl-div"]', {timeout:0});
-        // await page.waitForSelector('[class="j_drop-handler"]', {timeout:0});
-        cooook = await page.cookies();
-        await console.log(cooook);
+        pre_cookie = await page.cookies();
+        af_cookie = []
+        for (var i=0; i<pre_cookie.length;i++){
+            await af_cookie.push({'name':pre_cookie[i]['name'], 'value':pre_cookie[i]['value']})
+        }
+        await console.log(af_cookie)
+        fs.readFile('cookie.json', 'utf8', function (err, data) {   // 判断文件是否存在，不存在就创建
+            if (err) console.log(err);
+            data = JSON.stringify(af_cookie)
+            fs.writeFileSync('cookie.json', data, 'utf8', (err) => {    // 获取登录过后的cookie并保存
+                if (err) throw err;
+                console.log('done');
+            });
+        });
     }
 
-	// await page.type('[id="scform_srchtxt"]', '漏洞');
-	// // await page.waitForTimeout(3000);
-	// // await page.keyboard.down('Enter');
-	// // await page.keyboard.up('Enter');
-    // await page.keyboard.press('Enter');
-	// // await page.waitForTimeout(15000);
-	// await console.log(page.url());
-
-	// fs.readFile('/home/antx/Code/SOC-TI/ti_ds/spider/information/ichunqiu.json', 'utf8', function (err, data) {
-    //         if (err) console.log(err);
-    //         data = JSON.stringify(page.url());
-    //         fs.writeFileSync('/home/antx/Code/SOC-TI/ti_ds/spider/information/ichunqiu.json', data, 'utf8', (err) => {
-    //             if (err) throw err;
-    //             logger.error(err);
-    //             console.log('done');
-    //         });
-    //     });
-
-    // await page.close();
-    // await console.log('页面已关闭')
-    // await browser.close()
-    // await random.Random(3,7)
-    // await console.log('浏览器已关闭')
+    await page.close();
+    await console.log('页面已关闭')
+    await browser.close()
+    await console.log('浏览器已关闭')
 })();
 
