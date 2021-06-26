08d42fb5d0a158e3bcea1e0ba58b6d9d89b4f995
git <wkaifeng2007@163.com>
2021-06-26 13:39:02 +0800

    update BcAppAPI-web at 2021-06-26 13:39

--- py3.code/BcAppAPI/web/api/app_user.py
+++ py3.code/BcAppAPI/web/api/app_user.py
@@ -7,7 +7,9 @@ from io import BytesIO
 from utils.exceptions.customs import InvalidPermissions, UnauthorizedAPIRequest, RecordNotFound
 from utils.services.base.base_func import *
 from utils.services.redis_db_connect.connect import *
+from utils.services.base.SnowFlake import IdWorker
 from web.models.appuser_models import *
+from app.handler.user_handler import *
 
 # logger.add(sink='logs/app_user.log',
 #            level='ERROR',
@@ -33,6 +35,8 @@ record_db = db_connection('bc-app', 'records')
 share_buy_db = db_connection('bc-app', 'share_buy_code')
 redis_service = redis_connection(redis_db=0)
 
+id_worker = IdWorker(0, 0)
+
 @logger.catch(level='ERROR')
 @router.get('/all')
 async def get_all_users():
@@ -41,11 +45,76 @@ async def get_all_users():
 @logger.catch(level='ERROR')
 @router.post('/add_user')
 async def add_user(request: Request, add_info: AddUser):
-	pass
+	user_id = id_worker.get_id()  # 生成唯一用户id
+	now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+	if add_info.nickname in user_db.dep_data('nickname'):
+		return msg(status='error', data='User already exists!')
+	pcode = promo_code(user_id)
+	user_info = {
+		'user_id': user_id,
+		'nickname': add_info.nickname,
+		'email': add_info.email,
+		'phone': add_info.phone,
+		'password': result_hash(add_info.init_password),
+		'created_time': now_time,
+		'last_login_time': '',
+		'last_login_ip': '',
+		'access_token': '',
+		'promo_code': pcode,
+		'is_active': True,
+		'is_verified': True,
+		'is_superuser': False,
+		'is_logged_in': False
+	}
+	asset_info = {
+		'user_id': user_id,
+		'asset': {
+			'usdt': {
+				'all': 0,
+				'sum_reward': 0,
+				'today_reward': 0,
+			},
+			'miner': [],
+			'team_miner': [],
+			'share': 0
+		}
+	}
+	profile_info = {
+		'user_id': user_id,
+		'base_info': {
+			'profile': {
+				'nickname': add_info.nickname,
+				'sex': 'male',
+				'area': 'china',
+				'intro': '这个家伙很懒，什么都没留下'
+			},
+			'share': {
+				'invite_code': 'INTIAL',
+				'promo_code': pcode,
+			}
+		}
+	}
+	dnk_info = {
+	    "user_id" : user_id,
+	    "own_code" : pcode,
+	    "pre1_code" : "INTIAL",
+	    "pre2_code" : "INTIAL",
+	    "pre3_code" : "INTIAL",
+	    "af1_code" : [],
+	    "af2_code" : []
+	}
+	user_db.insert_one_data(user_info)
+	user_info_db.insert_one_data(profile_info)
+	asset_db.insert_one_data(asset_info)
+	dnk_db.insert_one_data(dnk_info)
+	return msg(status='success', data='Add new app user successfully!')
 
 @logger.catch(level='ERROR')
 @router.get('/{user_id}')
 async def get_user(user_id):
+	user_id = int(user_id)
+	if user_id not in user_db.dep_data('user_id'):
+		return msg(status='error', data='User was not exist!')
 	user_info = user_db.find_one({'user_id': user_id})
 	promo_invite_info = user_info_db.find_one({'user_id': user_id})
 	return_info = {
@@ -53,8 +122,8 @@ async def get_user(user_id):
 		'nickname': user_info['nickname'],
 		'phone': user_info['phone'],
 		'email': user_info['email'],
-		'invite_code': promo_invite_info['share']['invite_code'],
-		'promo_code': user_info['share']['promo_code'],
+		'invite_code': promo_invite_info['base_info']['share']['invite_code'],
+		'promo_code': promo_invite_info['base_info']['share']['promo_code'],
 		'register_time': user_info['created_time'],
 		'last_login_time': user_info['last_login_time'],
 		'last_login_ip': user_info['last_login_ip'],
@@ -67,10 +136,19 @@ async def get_user(user_id):
 
 @logger.catch(level='ERROR')
 @router.post('/update_user')
-async def update_user():
-	pass
+async def update_user(request: Request, update_info: UpdateUser):
+	updates = {}
+	if update_info.update_info:
+		for inx, (k, v) in enumerate(dict(update_info.update_info).items()):
+			if v:
+				updates[k] = v
+	user_db.update_one({'user_id': update_info.user_id}, updates)
+	return msg(status='success', data='Updated user info successfully')
 
 @logger.catch(level='ERROR')
 @router.post('/delete_user')
-async def delete_user():
-	pass
+async def delete_user(request: Request, delete_info: DeleteUser):
+	if delete_info.user_id not in user_db.dep_data('user_id'):
+		return msg(status='error', data='User not exist!')
+	user_db.delete_one({'user_id': delete_info.user_id})
+	return msg(status='success', data='User deleted successfully')
--- py3.code/BcAppAPI/web/models/appuser_models.py
+++ py3.code/BcAppAPI/web/models/appuser_models.py
@@ -2,17 +2,25 @@ from pydantic import BaseModel
 from typing import Optional
 
 class AddUser(BaseModel):
-    email: Optional[str]
-    phone: Optional[str]
+    email: Optional[str] = ''
+    phone: Optional[str] = ''
     nickname: str
     init_password: str = '123456789'
 
-class GetUser(BaseModel):
-    user_id: int
+class UpdateInfo(BaseModel):
+    nickname: Optional[str]
+    email: Optional[str]
+    phone: Optional[str]
+    reset_password: Optional[bool]
+    is_logged_in: Optional[bool]
+    is_active: Optional[bool]
+    is_verified: Optional[bool]
+    member_status: Optional[str]
+    level_status: Optional[str]
 
 class UpdateUser(BaseModel):
     user_id: int
-    update_info: dict
+    update_info: UpdateInfo
 
 class DeleteUser(BaseModel):
     user_id: int
